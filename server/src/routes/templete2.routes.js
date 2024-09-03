3dea1955b77b9c2381c436c56a9ef625e8edf786
Chandan Polai <136923981+ChandanPolai@users.noreply.github.com>
2024-09-03 17:44:36 +0530

    succesfully complette all the resume rotes

--- server/src/routes/templete2.routes.js
+++ server/src/routes/templete2.routes.js
@@ -1,6 +1,8 @@
 import { 
     insertDummyData, 
     editResume, 
+    uploadImage,
+    getPermanentResume,
     updateResumeByResumeId,
     updateUserAvatar ,
     getResumesByUserId,
@@ -15,22 +17,36 @@ import { Router } from "express";
 
 const router = Router();
 
+// ## insert dummy data in db routes
 router.route("/resume-data").get(insertDummyData);
+
+//  ##for create resume form the dummy data
 router.route("/edit-resume").post(verifyJWT,editResume);
+
+//  ## upload resume image first time store in user schema
+router.route("/upload-image").post(verifyJWT, upload.single("image"),uploadImage);
+
+//  ## update the resume using resumeid
 router.route("/resume-edit/:resumeId").patch(verifyJWT,updateResumeByResumeId);
 
+// ## update the resume image using resumeid
 router
 .route("/image/:resumeId")
 .patch(verifyJWT, upload.single("image"), updateUserAvatar);
 
-// /resume/:resumeId/avatar
-
+//  ## get the resume using loggedin userid 
 router.route("/Allresume").get(verifyJWT,getResumesByUserId);
-// router.route("/usersPermanentDetais").get(verifyJWT,);
+
+// ## get the resume when the user permentdetail {TRUE}
+router.route("/usersPermanentDetais").get(verifyJWT,getPermanentResume);
+
+//  ## delete the ALLresumes using LOGGEDINuserid
 router.route("/delete-Allresume").delete(verifyJWT,deleteAllResumesByUserId);
 
-
+//  ## get the resume by resumeid
 router.route("/resume-data/:resumeId").get(verifyJWT,getResumeById);
+
+//  ## delete the resume using resumeid
 router.route("/delete/resume/:resumeId").delete(verifyJWT,deleteResumeById);
 
 

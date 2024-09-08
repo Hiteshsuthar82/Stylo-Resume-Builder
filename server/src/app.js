1b76384650d2c6d28b4a768c843344e3a5163428
Hitesh <hiteshsuthar8255@gmail.com>
2024-09-08 10:24:47 -0700

    fix misc

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://stylo-resume-builder.vercel.app/",],
+    origin: ["https://stylo-resume-builder.vercel.app",],
     credentials: true,
   })
 );
